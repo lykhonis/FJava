02502e6c54d534fcdfc59eb41629231aa3e9cc15
Vladimir Lichonos <vladimirlichonos@gmail.com>
2012-01-27 21:50:32 -0800

    Lazy fix

--- src/com/fjava/library/Lazy.java
+++ src/com/fjava/library/Lazy.java
@@ -4,16 +4,24 @@ public class Lazy<T> {
 
 	private T value;
 	private F1<T> f;
+	private boolean called;
 
 	public Lazy(F1<T> f) {
 		this.f = f;
+		this.called = false;
 	}
 
 	public T invoke() {
-		if (value == null) {
+		if (called)
+			return value;
+
+		try {
 			value = f.invoke();
+		} finally {
 			f = null;
+			called = true;
 		}
+
 		return value;
 	}
 }
